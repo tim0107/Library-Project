c2c705d802fc3d5a4da3bd67686cab5188843a6c
tim0107 <truonggoiii2003@gmail.com>
2026-04-17 13:36:33 +0800

    fix cros 2

--- src/app.js
+++ src/app.js
@@ -44,8 +44,6 @@ const corsOptions = {
 
 // middleware
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
